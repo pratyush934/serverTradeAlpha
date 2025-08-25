5f988fae56302f71ef98eb964b900d07f51c0f43
Pratyush <oggycandle786@gmail.com>
2025-08-26 00:53:55 +0530

    Adding imp stuff

--- models/user_model.go
+++ models/user_model.go
@@ -45,3 +45,46 @@ func (u *User) CreateUser() (*User, error) {
 	}
 	return u, nil
 }
+
+func GetUserById(id string) (*User, error) {
+	var user User
+	if err := database.DB.Where("user_id = ?").First(&user).Error; err != nil {
+		log.Error().Err(err).Msg("Issue lie in the user_model/GetUserById")
+		return nil, err
+	}
+	return &user, nil
+}
+
+func GetUserByEmail(id string) (*User, error) {
+	var user User
+	if err := database.DB.Where("email = ?").First(&user).Error; err != nil {
+		log.Error().Err(err).Msg("Issue lie in the user_model/GetUserByEmail")
+		return nil, err
+	}
+	return &user, nil
+}
+
+func UpdateUser(user User) error {
+	if err := database.DB.Updates(&user).Error; err != nil {
+		log.Error().Err(err).Msg("Issue lie in the user_model/UpdateUser")
+		return err
+	}
+	return nil
+}
+
+func GetAllUsers() ([]User, error) {
+	var users []User
+	if err := database.DB.Find(&users).Error; err != nil {
+		log.Error().Err(err).Msg("Issue lie the user_model/GetAllUsers")
+		return nil, err
+	}
+	return users, nil
+}
+
+func DeleteUserById(id string) error {
+	return database.DB.Where("user_id = ?", id).Delete(&User{}).Error
+}
+
+func DeleteUserByEmail(email string) error {
+	return database.DB.Where("email = ?", email).Delete(&User{}).Error
+}
