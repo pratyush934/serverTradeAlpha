5b8abf6ae6421ed3df158ec22bfaa0220980f0c5
Pratyush <oggycandle786@gmail.com>
2025-08-29 12:28:16 +0530

    adding imp

--- controller/portfolio_controller.go
+++ controller/portfolio_controller.go
@@ -15,7 +15,6 @@ CreatePortfolio
 
 GetPortfolioById
 
-GetUserPortfolios
 
 UpdatePortfolio
 
@@ -56,5 +55,85 @@ func CreatePortfolio(c echo.Context) error {
 }
 
 func GetPortFolioById(c echo.Context) error {
-	return nil
+	userId := c.Get("userId").(string)
+
+	if userId == "" {
+		return util.NewAppError(http.StatusUnauthorized, types.StatusUnauthorized, "not able to get the userId , GetPortFolioById", nil)
+	}
+
+	portId := c.Param("id")
+
+	if portId == "" {
+		return util.NewAppError(http.StatusBadRequest, types.StatusBadRequest, "please provide portfolioId", nil)
+	}
+
+	portFolioById, err := models.GetPortFolioById(portId)
+
+	if err != nil {
+		return util.NewAppError(http.StatusBadRequest, types.StatusBadRequest, "not able te get the protFolio by Id", err)
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"portfolio": portFolioById,
+	})
+}
+
+func UpdatePortFolioStock(c echo.Context) error {
+
+	userId := c.Get("userId").(string)
+
+	if userId == "" {
+		return util.NewAppError(http.StatusUnauthorized, types.StatusUnauthorized, "not able to get the userid, UpdateThePortFolio", nil)
+	}
+
+	portId := c.Param("id")
+
+	if portId == "" {
+		return util.NewAppError(http.StatusBadRequest, types.StatusBadRequest, "not able to get the portId, UpdatePortfolio", nil)
+	}
+
+	var portfolio dto.PortFolioDTO
+
+	if err := c.Bind(&portfolio); err != nil {
+		return util.NewAppError(http.StatusBadRequest, types.StatusBadRequest, "not able to bind the portfolio, UpdatePortFolio", err)
+	}
+
+	portFolioById, err := models.GetPortFolioById(portId)
+
+	if err != nil || portFolioById == nil {
+		return util.NewAppError(http.StatusBadRequest, types.StatusBadRequest, "not able te get the protFolio by Id", err)
+	}
+
+	portFolioById.Title = portfolio.Title
+	portFolioById.Name = portfolio.Name
+	portFolioById.Description = portfolio.Description
+
+	if err := models.UpdatePortFolio(*portFolioById); err != nil {
+		return util.NewAppError(http.StatusBadRequest, types.StatusBadRequest, "not able to get the Update portfolio", err)
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"update_status": http.StatusOK,
+	})
+
+}
+
+func DeletePortFolio(c echo.Context) error {
+
+	userId := c.Get("userId").(string)
+
+	if userId == "" {
+		return util.NewAppError(http.StatusUnauthorized, types.StatusUnauthorized, "not able to get the userId", nil)
+	}
+
+	portId := c.Param("id")
+
+	if err := models.DeletePortfolioById(portId); err != nil {
+		return util.NewAppError(http.StatusBadRequest, types.StatusBadRequest, "not able to delete the portfolio by portId", err)
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "PortFolioDelete successfully",
+	})
+
 }
